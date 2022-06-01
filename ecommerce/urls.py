0df18859fcbd3d2bab858c9fb18e77d3e1c07e7c
IbrarAman <ibraraman221@gmail.com>
2022-06-01 19:08:34 +0500

    Product Order and Payments

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,8 +21,11 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chaining/', include('smart_selects.urls')),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    # Orders
+    path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
